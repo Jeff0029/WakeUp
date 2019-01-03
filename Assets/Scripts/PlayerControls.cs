d9376dfd3ea89b1a1f595036806c6b3754ca23b8
Jeff0029 <jeanfrancoisvienneau@gmail.com>
2019-01-02 20:41:45 -0500

    Added FMOD and sample scene
    Added the fmod plugin for Unity.  Replacing the default audio system.
    
    Added model/mat/texture to create a small sample scene to fiddle in.

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class PlayerControls : MonoBehaviour
+{
+    CharacterController controls;
+    [Header("Jump Settings")]
+    [SerializeField]
+    internal float jumpForce;
+    private Vector3 jump;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls = GetComponent<CharacterController>();
+        jump = new Vector3(0, jumpForce, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetButton("Jump")) {
+            controls.Move(jump);
+        }
+    }
+}
