dae4841d48f84e4b5d94d6e9a09b1a2a875751de
jeff0029 <blackhat63@gmail.com>
2019-02-09 20:25:35 -0500

    Added icon switching logic when a selectable item is hovered over

--- Assets/Scripts/MainMenu/SelectionRayCast.cs
+++ Assets/Scripts/MainMenu/SelectionRayCast.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum SelectionAction
 {
@@ -12,8 +14,55 @@ public enum SelectionAction
 
 public class SelectionRayCast : MonoBehaviour
 {
+    [SerializeField]
+    internal RawImage cursor;
+    private Texture defaultIcon;
+    private SelectableItem curSelection; 
+    public SelectableItem CurSelection
+    {
+        get { return CurSelection; }
+    }
+    private Dictionary<SelectionAction, Texture> selectionTextures = new Dictionary<SelectionAction, Texture>();
+
+    private void Start()
+    {
+        // Force MenuSelector
+        gameObject.layer = LayerMask.NameToLayer("MenuSelector");
+        defaultIcon = cursor.texture;
+
+        foreach (int selection in Enum.GetValues(typeof(SelectionAction)))
+        {
+            Texture selectionIcon = Resources.Load<Texture>("Textures/Icons/Selections/" + Enum.GetName(typeof(SelectionAction), selection));
+            selectionTextures.Add((SelectionAction)selection, selectionIcon);
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("worked");
+        // Object collided with should be under menu selection
+        SelectableItem item = other.GetComponent<SelectableItem>();
+        if (item == null)
+        {
+            Debug.LogError("Missing SelectableItem component on " + item.name);
+        } else if (curSelection == null) //If we're selecting something currently, stay on that selection
+        {
+            curSelection = item;
+            cursor.texture = selectionTextures[item.selection];
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        // Make sure to get the gameObject.  Each components has it's own ID!
+        if (other.gameObject.GetInstanceID() == curSelection.gameObject.GetInstanceID())
+        {
+            curSelection = null;
+            cursor.texture = defaultIcon;
+        }
+    }
+
+    private IEnumerator IsSelectionStillActive()
+    {
+
+        yield return null;
     }
 }
