59bd1c5a049d3dd69e5b4b2a711b436c69f1f7f9
jeff0029 <blackhat63@gmail.com>
2019-02-24 19:18:27 -0500

    Added starting version for managing player entering/leaving room

---
+++ Assets/Scripts/Photon/RoomButler.cs
@@ -0,0 +1,54 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomButler : MonoBehaviourPunCallbacks, IInRoomCallbacks
+{
+    //Room info
+    public static RoomButler room;
+    private PhotonView PV;
+
+    public bool isGameLoaded;
+    public int currentScene;
+
+    //Player info
+    Player[] photonPlayers;
+    public int playerInRoom;
+    public int myNumberInRoom;
+
+    public int playerInGame;
+
+    //Delayed start
+    private bool readyToCount;
+    private bool readyToStart;
+    public float startingTime;
+    private float lessThanMaxPlayers;
+    private float aMaxPlayer;
+    private float timeToStart;
+
+    private void Awake()
+    {
+        if (RoomButler.room == null)
+        {
+            RoomButler.room = this;
+        } else if (RoomButler.room != this)
+        {
+            Destroy(RoomButler.room.gameObject);
+            RoomButler.room = null;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
